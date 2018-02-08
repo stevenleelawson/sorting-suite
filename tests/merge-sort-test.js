5654db4aa4f69b40e0df4b662114622e23e09f15
Steven Lawson <unluckygamblermusic@gmail.com>
2018-02-07 18:00:19 -0700

    changed sort to merge sort

--- tests/merge-sort-test.js
+++ tests/merge-sort-test.js
@@ -1,28 +1,28 @@
 const {expect, assert} = require('chai');
-const sort = require('../lib/merge-sort.js');
+const mergeSort = require('../lib/merge-sort.js');
 // const merge = require('../lib/merge-sort.js');
 
-describe('sort', function() {
+describe('mergeSort', function() {
   it('should be a function', function(){
-    assert.isFunction(sort, 'probably should be a function');
+    assert.isFunction(mergeSort, 'probably should be a function');
   })
   it('should sort a small array of numbers', function(){
     let numbers = [5,4, 500003, 3, 2, 1, 20, 88, 22];
 
-    assert.deepEqual(sort(numbers),[ 1, 2, 3, 4, 5, 20, 22, 88, 500003 ])
+    assert.deepEqual(mergeSort(numbers),[ 1, 2, 3, 4, 5, 20, 22, 88, 500003 ])
     })
   it('should sort letters', function(){
     let letters = ['d', 'b', 'a', 'c', 'e'];
 
-    assert.deepEqual(sort(letters),[ 'a', 'b', 'c', 'd', 'e'])
+    assert.deepEqual(mergeSort(letters),[ 'a', 'b', 'c', 'd', 'e'])
     })
   it('should sort positive and negative numbers', function(){
     let numbers = [-3, -5, 4, 1, 2];
 
-    assert.deepEqual(sort(numbers),[-5, -3, 1, 2, 4])
+    assert.deepEqual(mergeSort(numbers),[-5, -3, 1, 2, 4])
     })
   it('should sort an array of random numbers', function(){
-    let sortedArray = sort(generateRandomNumbers(33, 200));
+    let sortedArray = mergeSort(generateRandomNumbers(33, 200));
 
     function generateRandomNumbers( count, maxVal ) {
       const array = [];
@@ -38,7 +38,7 @@ describe('sort', function() {
     }
     })
     it('should sort an array of large numbers', function(){
-      let sortedArray = sort(generateRandomNumbers(200, 9000));
+      let sortedArray = mergeSort(generateRandomNumbers(200, 9000));
 
       function generateRandomNumbers( count, maxVal ) {
         const array = [];
